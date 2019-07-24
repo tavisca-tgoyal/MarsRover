8f9e9200bbdf7a7197f8b8ebf754df5ad1ade778
mgandhe <mgandhe@RENTLAPT0099>
2019-07-24 18:08:39 +0530

    Added Map_initilization Test Method.

---
+++ MarsRover.Tests/MapFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MarsRover;
+using Xunit;
+using FluentAssertions;
+
+namespace MarsRover.Tests
+{
+    public class MapFixture
+    {
+
+        [Fact]
+        public void Map_Initialization_Test()
+        {
+            var map = new Map();
+            map.width = 100;            
+            map.length = 100;
+            map.length.Should().Be(100);
+            map.width.Should().Be(100);
+        }
+    }
+}
---
+++ MarsRover/Map.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Map
+    {
+        public int width { get; set; }
+        public int length { get; set; }
+
+    }
+}
